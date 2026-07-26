4618fa7675fc12923ec43dbdd7630f4accc037f0
arhan <arhankashyap21@gmail.com>
2026-07-26 20:08:00 +0530

    varibles and multiple assignment

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("hello,git!")
+age = 18
+
+print(age)
+
+sentence = "My name is Arhan"
+print(sentence)
+
+Bob=Arhan=Mike=18 
+print(Bob)
+print(Arhan)
+print(Mike)
+
+name,age = "Arhan", 18
+print(name)
+print(age)
