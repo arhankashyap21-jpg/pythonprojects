5f944fd418f34eeb79f175dfd2f6c5c02115358b
arhan <arhankashyap21@gmail.com>
2026-07-26 22:02:53 +0530

    list add,delete,length,max and min

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-shopping_list = ["milk","eggs","bread","butter"]
-print("Shopping List:")
-print(shopping_list[0])
-print(shopping_list[1])
-print(shopping_list[0:2])
+shopping_list = ["milk", "eggs", "bread", "butter"]
+shopping_list.append("cheese")
+print(shopping_list)
 
+del shopping_list[1]
+print(shopping_list)
+
+print(len(shopping_list))
+
+shopping_list2 = ["apples", "bananas", "oranges"]
+print(shopping_list + shopping_list2)
+
+list_num = [1, 2, 3, 4, 5]
+
+print(max(list_num))
+print(min(list_num))
