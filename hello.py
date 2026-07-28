794aa600be79b965d52e8b5ef90c28123b628660
arhan <arhankashyap21@gmail.com>
2026-07-28 10:31:13 +0530

    functions

--- hello.py
+++ hello.py
@@ -1,8 +1,23 @@
-try:
-    if name >3:
-        print("hello")
-except:
-    print("error has been detected in your code")
-    
-          
-    
+def hello_world():
+    print("hello world")
+
+hello_world()
+
+def greeting(name):
+    print("hi"+name+"!")
+greeting("Arhan")
+
+def add (num1,num2):
+    print(num1+num2)
+add(10,15)
+
+def add(num1,num2):
+    return num1+num2
+num_sum =add(12,34)
+print(num_sum)
+
+def add(num1,num2):
+    return num1+num2
+def mul(num1,num2):
+    return num1*num2
+print(mul(add(1,2),add(3,4)))
