1aefb16b1426c98b618800bbd3122bac5b9b3a66
arhan <arhankashyap21@gmail.com>
2026-07-29 09:09:16 +0530

    classes and object

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
-print("hello"+str(100))
-print(int(123+345))
-print(float("123.45"))
+class person:
+    def __init__(self,name,age):
+      self.name = name
+      self.age = age
+
+    def getName(self):
+       return self.name
+    def getAge(self):
+       return self.age
+
+p1= person("bob",22)
+
+print(p1.getName())
+print(p1.getAge()) 
