95a6b41c556e56a55cbe8b984c5699d1f3cdb510
arhan <arhankashyap21@gmail.com>
2026-08-04 11:02:46 +0530

    truthtable

---
+++ truthtable.py
@@ -0,0 +1,13 @@
+e = True or False
+
+# Truth table of 'or'
+print("True or false is", True or False)
+print("True or true is", True or True)
+print("False or true is", False or True)
+print("False or false is", False or False)
+
+# Truth table of 'and'
+print("True and false is", True and False)
+print("True and true is", True and True)
+print("False and true is", False and True)
+print("False and false is", False and False)
