e77a10c0ddc209d29520f323024518b69a74e142
arhan <arhankashyap21@gmail.com>
2026-08-05 10:56:33 +0530

    typefunction

---
+++ type.py
@@ -0,0 +1,20 @@
+a = 31
+t= type(a) # class <int>
+
+print(t)
+
+a = 32.5
+t= type(a) # class <float>
+
+print(t)
+
+a = "Arhan"
+t= type(a) # class <str>
+
+print(t)
+
+a = "31.2"
+b = float(a) # convert string to float
+t = type(b) 
+
+print(t)
